b444243ffaef84cb7d5406ad3284d316982cf3f8
Shubhit Sardana <91517488+Critical07@users.noreply.github.com>
2021-10-31 01:27:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+AOS.init();
+
+window.addEventListener("scroll", ()=>{
+    let value = window.scrollY;
+    if (window.scrollY > 730){
+        document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.79)";
+    }
+    else{
+        document.querySelector("nav").style.backgroundColor = "transparent";
+    }
+})
